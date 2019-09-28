cd431bdc463e70f36c0e49070a616b4a49c1b820
Nav <navyasuri@nyu.edu>
2019-09-28 17:33:46 -0400

    Added random questions

--- app/main-app.py
+++ app/main-app.py
@@ -2,19 +2,21 @@ from flask import Flask, request, render_template, url_for
 import os
 app = Flask(__name__)
 
+users = dict()
+
 @app.route('/')
 def home_page(): 
     return render_template("home.html") 
 
 @app.route('/create')
 def show_another():
-    # return render_template("create.html")
-    return "Create page"
+    return render_template("create.html")
+    # return "Create page"
 
 @app.route('/join')
 def join_another():
-    # return render_template("join.html")
-    return "Join page"
+    return render_template("join.html")
+    # return "Join page"
 
 # @app.route('/')
 #     return render_template()
