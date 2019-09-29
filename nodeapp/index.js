9ed380b7b763fedcb046bba57cf9c5e6ca44a9c4
Nav <navyasuri@nyu.edu>
2019-09-29 05:15:22 -0400

    removed old code

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -100,32 +100,3 @@ io.on('connection', (socket) => {
         io.in(code).emit("startClientGame")
     })
 })
-
-// app.get('/:name', function(req, res){
-//     name = req.params.name;
-//     res.sendFile(path.join(__dirname, "/index.html"));
-// });
-
-// // socket
-// io.sockets.on("connection", function(socket){
-//     users[socket.id] = name;
-//     // node
-//     socket.on("nRoom", function(room){
-//         socket.join(room);
-//         socket.broadcast.in(room).emit("node new user", users[socket.id] + " new user has joined");
-//     });
-
-//     socket.on("node new message", function(data){
-//         io.sockets.in("nRoom").emit('node news', users[socket.id] + ": "+ data);
-//     });
-
-//     // python
-//     socket.on("pRoom", function(room){
-//         socket.join(room);
-//         socket.broadcast.in(room).emit("python new user", users[socket.id] + " new user has joined");
-//     });
-
-//     socket.on("python new message", function(data){
-//         io.sockets.in("pRoom").emit('python news', users[socket.id] + ": "+ data);
-//     });
-// });
