5e96dd7f77c58b98f6903f18e7eed3402992a743
Nav <navyasuri@nyu.edu>
2019-09-29 01:05:12 -0400

    More commits to index

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -5,20 +5,31 @@ const server = app.listen(9000);
 const io = socketIO(server);
 const path = require("path");
 
-var users = {};
+var user_rooms = [];
 var name = '';
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/html/home.html"))
 })
 
-app.get('/join', (req, res)=>{
+app.get('/join', (req, res) => {
     res.sendFile(path.join(__dirname, "/html/join.html"))
 })
 
-app.get('/create', (req, res)=>{
-    res.sendFile(path.join(__dirname, "/html/join.html"))
+app.get('/create', (req, res) => {
+    var code = makeid(8);
+    res.sendFile(path.join(__dirname, "/html/create.html"))
 })
+
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 // app.get('/:name', function(req, res){
 //     name = req.params.name;
